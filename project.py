3c18b75df2db5d8bda9499f48d026ab7846067d2
titotiago <amorim.tiago@hotmail.com>
2020-08-13 22:35:26 -0300

    Limpeza de código

--- project.py
+++ project.py
@@ -14,15 +14,7 @@ def main():
   tabela = [[0 for i in range(n+2)] for i in range(m+4)]
 
   #setando os dados iniciais da tabela 
-  for i in range(0,int(m)):
-   line = f.readline()
-   j = 0
-   for s in line.split(' '):
-    num = float(s)
-    tabela[i][j] = num
-    j += 1
-
-  for i in range(m, int(m+4)):
+  for i in range(0, int(m+4)):
    line = f.readline()
    j = 0
    for s in line.split(' '):
@@ -32,11 +24,8 @@ def main():
 
   # Create the mip solver with the CBC backend.
   solver = pywraplp.Solver.CreateSolver('simple_mip_program', 'CBC')
-
   infinity = solver.infinity()
   precos = tabela[m+3] #preços finais
-
-
 
   #variaveis do solver
   produtos = [0 for i in range(n)] #quantidades de cada produto
@@ -64,21 +53,15 @@ def main():
     constraint.SetCoefficient(booleanos[i],1)
     constraint.SetCoefficient(produtos[i],-1)
 
-
-
   #Restrições de demanda
-
   for i in range(n): #restrições de demanda max -- produtos[i] <= dmax
    constraint = solver.RowConstraint(0, tabela[m+2][i], '')  
    constraint.SetCoefficient(produtos[i], 1)
-
 
   for i in range(n): #restrições de demanda min -- produtos[i] >= dmin*booleano
     constraint = solver.RowConstraint(0,infinity,'')
     constraint.SetCoefficient(produtos[i],1)
     constraint.SetCoefficient(booleanos[i],-tabela[m+1][i])
-
-
 
   #Restrição do somatório de horas
   constraint = solver.RowConstraint(0,B,'')
@@ -86,14 +69,12 @@ def main():
    constraint.SetCoefficient(booleanos[i],T)  #Decrementando quando troca de produção
    constraint.SetCoefficient(produtos[i],tabela[m][i])
   
-
   # Restrição de quantidade de cada material
   for i in range(m):  #produto*qtd_material <= N_lotes*tamanho_lote 
     constraint = solver.RowConstraint(0,tabela[i][n]-1,'')
     constraint.SetCoefficient(n_lotes[i],-tabela[i][n])
     for j in range(n):
      constraint.SetCoefficient(produtos[j],tabela[i][j])
-
 
   #Função objetivo
   #max SVx*px - F - SN_lotes*Cx 
@@ -126,7 +107,6 @@ def main():
    print("\n")
    for i in range(m):
     print("> Foram comprados",n_lotes[i].solution_value(), "lotes do material", i)
-  
    print("\nHoras fazendo trocas:", horas_troca)
    print("\nTotal de Horas: ", horas+horas_troca, "/", B )
    print()
